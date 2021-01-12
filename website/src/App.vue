63a951d701cf46023450e62d807bf85fe0f9b4df
Alexandre Falardeau <asfal@AsFal-Laptop.local>
2021-01-11 20:06:34 -0500

    feat: basic image search UI

--- website/src/App.vue
+++ website/src/App.vue
@@ -5,56 +5,117 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-toolbar-title>Shopify Application</v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+
+ <v-card
+    color="grey lighten-4"
+  >
+<v-container>
+    <v-row>
+      <v-col>
+      <v-select
+        v-model="selection"
+        :items="items"
+        label="Search By"
+      ></v-select>
+      </v-col>
+
+  <v-col
+    cols="12"
+    md="8"
+  >
+      <v-text-field
+        v-if="selection==='Text'"
+        v-model="text"
+        hide-details
+        prepend-icon="mdi-magnify"
+        single-line
+      ></v-text-field>
+
+    <v-combobox
+      v-if="selection==='Tags'"
+      v-model="tags"
+      :items="tagOptions"
+      hide-selected
+      label="Add some tags"
+      multiple
+      persistent-hint
+      small-chips
+    >
+      <template v-slot:no-data>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              No results matching "
+              <strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-combobox>
+
+      <v-file-input
+        v-if="selection==='Image'"
+        label="Image"
+      ></v-file-input>
+    </v-col>
+    </v-row>
+</v-container>
+
+    </v-card>
+
+<v-container>
+        <v-row>
+          <v-col
+            v-for="n in 9"
+            :key="n"
+            class="d-flex child-flex"
+            cols="2"
+          >
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row
+                  class="fill-height ma-0"
+                  align="center"
+                  justify="center"
+                >
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-col>
+        </v-row>
+</v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    selection: 'Tags',
+    items: [
+      'Image',
+      'Text',
+      'Tags',
+    ],
+    tagOptions: ['Gaming', 'Programming', 'Vue', 'Vuetify'],
+    tags: [],
+    search: null,
   }),
 };
 </script>
