cb91902813632097dcef7b73625f08a483d624fe
a <a@a.com>
2021-01-14 05:14:18 +0000

    refactor: remove debug deployment

--- deployments/application/demo-local/start.debug.sh
+++
@@ -1,9 +0,0 @@
-
-# !/usr/bash/bin
-
-docker-compose \
-    -f deployments/application/demo-local/docker-compose.application.yaml \
-    -f deployments/deepdetect/local-images/docker-compose.deepdetect.yaml \
-    -f deployments/elasticsearch/demo/docker-compose.elasticsearch.yaml \
-    up --build -d api 
- 
