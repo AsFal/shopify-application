a117759bb90be8fafca06e068121ca871f6fe831
Alexandre Falardeau <asfal@AsFal-Laptop.local>
2021-02-14 14:40:04 -0500

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 cd ~/shopify-application
 
-docker kill ($docker -q)
-docker rm ($docker -aq)
+docker kill $(docker -q)
+docker rm $(docker -aq)
 
 bash deployments/application/demo-local/start.sh
