c8b4ae857e4d284744cd0dfaa804089ae4467df5
Alexandre Falardeau <asfal@AsFal-Laptop.local>
2021-02-14 20:15:27 -0500

    feat: change start deployment

--- deployments/application/demo-amazon/start.sh
+++ deployments/application/demo-amazon/start.sh
@@ -4,5 +4,5 @@ docker-compose \
     -f deployments/application/demo-amazon/docker-compose.application.yaml \
     -f deployments/deepdetect/default/docker-compose.deepdetect.yaml \
     -f deployments/elasticsearch/demo/docker-compose.elasticsearch.yaml \
-    up --build
+    up -d --build
  
